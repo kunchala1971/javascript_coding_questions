fb12ad7901d9202a60ae4875b8c3672d115e6ea6
kunchala1971 <kunchala1971@gmail.com>
2026-07-11 21:28:10 +0530

    today brushing up

--- RemoveDuplicatesinArray.js
+++ RemoveDuplicatesinArray.js
@@ -3,9 +3,11 @@ let nums=[1,2,3,4,5,1,2,3,4,5];
 
 console.log([...new Set(nums)]);
 
-// const uniq=[];
-// nums.forEach(element => {
-   
-// });
+const uniq=[];
+nums.forEach(element => {
+    if (!uniq.includes(element)) {
+        uniq.push(element);
+    }
+});
 
-// console.log(uniq);
+console.log(uniq);
