8eabae6086e4e43fce76d13314c3eac380269f3a
kunchala1971 <kunchala1971@gmail.com>
2026-07-12 20:57:24 +0530

    Object group

--- ObjectGroup1.js
+++ ObjectGroup1.js
@@ -1,35 +1,51 @@
 const employees = [
     {
-        name:"John",
-        desig:"Engg"
+        name: "John",
+        desig: "Engg"
     },
     {
-        name:"smith",
-        desig:"Manager"
+        name: "smith",
+        desig: "Manager"
     },
     {
-        name:"Michel",
-        desig:"Engg",
+        name: "Michel",
+        desig: "Engg",
     },
     {
-        name:"Ford",
-        desig:"Analyst"
+        name: "Ford",
+        desig: "Analyst"
     },
     {
-        name:"Robin",
-        desig:"Analyst"
+        name: "Robin",
+        desig: "Analyst"
     }
 ];
 
-const groupedByDesignation = employees.reduce((acc, employee) => {
-    const designation = employee.desig;
-    if(!acc[designation]){
-        acc[designation] = [];
-    }
-    acc[designation].push(employee);
-    return acc;
-}, {}); 
+// const groupedByDesignation = employees.reduce((acc, employee) => {
+//     const designation = employee.desig;
+//     if(!acc[designation]){
+//         acc[designation] = [];
+//     }
+//     acc[designation].push(employee);
+//     return acc;
+// }, {}); 
 
-console.log("TEst");
-console.log(groupedByDesignation);
 
+// console.log(groupedByDesignation);
+
+const result = Object.values(
+    employees.reduce((acc, { desig, name }) => {
+        if (!acc[desig]) {
+            acc[desig] = {
+                desig,
+                employees: []
+            };
+        }
+
+        acc[desig].employees.push(name);
+
+        return acc;
+    }, {})
+);
+
+console.log(result);
